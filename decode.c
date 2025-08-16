b6b7aea4e3e0281fc9fd006f2e0975f522f5bc55
flhz <flo@flhz.is>
2025-08-16 22:49:48 +0200

    move tests, use Operation struct

--- decode.c
+++ decode.c
@@ -5,14 +5,14 @@
 #include <stdbool.h>
 
 typedef enum OpCode {
-  MOVE_REGMEM_TOFROM_REG = 0b10001000,
-  MOVE_IMM_TO_REGMEM     = 0b11000110,
-  MOVE_IMM_TO_REG_8      = 0b10110000,
-  MOVE_IMM_TO_REG_16     = 0b10111000,
-  MOVE_MEM_TO_ACC        = 0b10100000,
-  MOVE_ACC_TO_MEM        = 0b10100010,
-  MOVE_REGMEM_TO_SEGREG  = 0b10001110,
-  MOVE_SEGREG_TO_REGMEM  = 0b10001100,
+  MOVE_IMM_TO_REG_8      = 0b10110000, // 4
+  MOVE_IMM_TO_REG_16     = 0b10111000, // 5
+  MOVE_REGMEM_TOFROM_REG = 0b10001000, // 6
+  MOVE_IMM_TO_REGMEM     = 0b11000110, // 7
+  MOVE_MEM_TO_ACC        = 0b10100000, // 7
+  MOVE_ACC_TO_MEM        = 0b10100010, // 7
+  MOVE_REGMEM_TO_SEGREG  = 0b10001110, // 8
+  MOVE_SEGREG_TO_REGMEM  = 0b10001100, // 8
 } OpCode;
 
 typedef enum Mode {
@@ -53,6 +53,10 @@ typedef struct Operation {
   char *src;
 } Operation;
 
+//OpCode getOpcode(unsigned char bits)
+//{
+//};
+
 char* parseRegBits(unsigned char regBits, unsigned char wordBit)
 {
     if (wordBit == 0b00000001) {
@@ -64,12 +68,12 @@ char* parseRegBits(unsigned char regBits, unsigned char wordBit)
 
 char* parseRmBits(
   unsigned char rmBits,
-  unsigned char wordBit,
+  bool word,
   unsigned char modBits,
   FILE *f
 ) {
     if (modBits == MOD_REG) {
-      if (wordBit == 0b00000001) {
+      if (word) {
         rmBits = rmBits | 0b00001000;
       }
       return registers[rmBits];
@@ -124,44 +128,38 @@ int main(int argc, char **argv)
   size_t readp;
   volatile uint8_t bytes[2];
 
-  bool debug = true;
-
+  bool debug = false;
   while ((readp = fread(&readIn, 2, 1, f)) != 0) {
     Operation op = {};
     op.bytes[0] = readIn >> 8;
     op.bytes[1] = readIn & 0xFF;
 
-    bytes[0] = readIn >> 8; // low
-    bytes[1] = readIn & 0xFF; // high
-
     if (debug) {
-      printf("%08b %08b    |    ", bytes[1], bytes[0]);
+      printf("%08b %08b  |  ", op.bytes[1], op.bytes[0]);
     }
 
-    //printf("%b %b\n", bytes[1], bytes[0]);
     // high bits
-    unsigned char opcodeBits = bytes[1] & 0b11111100;
-    unsigned char shortopcodeBits = bytes[1] & 0b11111000;
-    unsigned char directionBit = bytes[1] & 0b00000010; // 1=reg on left
-    unsigned char wordBit = bytes[1] & 0b00000001; // ~wide 1=16bits, 0=8bits
+    unsigned char opcodeBits = op.bytes[1] & 0b11111100;
+    unsigned char shortopcodeBits = op.bytes[1] & 0b11111000;
     // low bits
-    unsigned char modBits = bytes[0] & OP_MASK_MOD; // 11=reg to reg
-    unsigned char regBits = bytes[0] & OP_MASK_REG;
-    unsigned char rmBits = bytes[0] & OP_MASK_RM;
+    unsigned char regBits = op.bytes[0] & OP_MASK_REG;
+    unsigned char rmBits = op.bytes[0] & OP_MASK_RM;
 
-    bool dir = (directionBit & 0b00000010) != 0;
+    op.mode      = op.bytes[0] & OP_MASK_MOD; // Mode
+    op.direction = (op.bytes[1] & OP_MASK_DIR) != 0; // 1=reg on left
+    op.word      = (op.bytes[1] & OP_MASK_WRD) != 0; // ~wide 1=16bits, 0=8bits
 
     // parse
     if (opcodeBits == MOVE_REGMEM_TOFROM_REG) {
       op.opcode = MOVE_REGMEM_TOFROM_REG;
-      char *dst = parseRmBits(rmBits, wordBit, modBits, f);
-      char *src = parseRegBits(regBits >> 3, wordBit);
+      char *dst = parseRmBits(rmBits, op.word, op.mode, f);
+      char *src = parseRegBits(regBits >> 3, op.word);
 
-      if (modBits != MOD_REG) {
-        dir ? printf("mov %s, [%s]", src, dst)
+      if (op.mode != MOD_REG) {
+        op.direction ? printf("mov %s, [%s]", src, dst)
           : printf("mov [%s], %s", dst, src);
       } else {
-        dir ? printf("mov %s, %s", src, dst)
+        op.direction ? printf("mov %s, %s", src, dst)
           : printf("mov %s, %s", dst, src);
       }
     }
@@ -169,8 +167,8 @@ int main(int argc, char **argv)
       op.opcode = MOVE_IMM_TO_REG_8;
       char regMask = 0b00000111;
       char str[10];
-      sprintf(str, "%d", bytes[0]);
-      op.dst = parseRegBits(bytes[1] & regMask, 0);
+      sprintf(str, "%d", op.bytes[0]);
+      op.dst = parseRegBits(op.bytes[1] & regMask, 0);
       op.src = str;
 
       printf("mov %s, %s", op.dst, op.src);
@@ -182,13 +180,13 @@ int main(int argc, char **argv)
       fread(&additional, 1, 1, f);
       op.bytes[2] = additional;
       uint16_t data = 0;
-      data = (additional << 8) + bytes[0];
+      data = (additional << 8) + op.bytes[0];
       //printf(":: %016b, %08b %08b\n", data, additional, bytes[0]);
-      printf("mov %s, %d", parseRegBits(bytes[1] & regMask, 1), data);
+      printf("mov %s, %d", parseRegBits(op.bytes[1] & regMask, 1), data);
     }
     else {
       op.opcode = opcodeBits;
-      printf("unknown %08b, %08b %08b", opcodeBits, bytes[1], bytes[0]);
+      printf("??? %08b, %08b %08b", op.opcode, op.bytes[1], op.bytes[0]);
     }
 
     printf("\n");
